57a4525694556c5e5b542295e06af545cb27782a
kmdsuhail72 <kmdsuhail72@gmail.com>
2026-05-24 23:43:58 +0530

    Fix Terraform backend S3 region

--- aws-terraform-new/main.tf
+++ aws-terraform-new/main.tf
@@ -14,7 +14,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket  = "terraformbuckerforstatefiles" # create s3 bucket to store statefile
+    bucket  = "terraformbuckerforstatefiles1" # create s3 bucket to store statefile
     key     = "dev/terraform.tfstate"
     region  = "ap-southeast-2"
     encrypt = false
@@ -24,7 +24,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-2"
 }
 
 module "vpc" {
@@ -32,7 +32,7 @@ module "vpc" {
   vpc_cidr             = "10.0.0.0/16"
   vpc_name             = "project-vpc"
   cidr_public_subnet   = ["10.0.1.0/24", "10.0.2.0/24"]
-  eu_availability_zone = ["us-east-1a", "us-east-1b"]
+  eu_availability_zone = ["ap-southeast-2a", "ap-southeast-2b"]
 }
 
 module "security_group" {
@@ -44,7 +44,7 @@ module "security_group" {
 
 module "ec2" {
   source             = "./ec2"
-  ami_id             = "ami-04b4f1a9cf54c11d0" # Ubuntu 22.04 (us-east-1)
+  ami_id             = "ami-04b4f1a9cf54c11d0" # Ubuntu 22.04 (ap-southeast-2)
   instance_type      = "t2.micro"
   subnet_id          = element(module.vpc.public_subnet_ids, 0)
   security_group_ids = [module.security_group.sg_ec2_sg_ssh_http_id]
